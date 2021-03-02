184aacbb44e4fb819d5be5ec52dad9df9d3b8acd
schzrodriguez <74927530+schzrodriguez@users.noreply.github.com>
2021-03-02 17:57:25 +0100

    Add files via upload

---
+++ Modulo1Fase1.java
@@ -0,0 +1,23 @@
+
+public class Modulo1Fase1 {
+
+	public static void main(String[] args) {
+		
+		String nom, cognom1, cognom2;
+		
+		int dia, mes, any;
+		
+		nom = "Sergio";
+		cognom1 = "Sanchez";
+		cognom2 = "Rodriguez";
+		
+		dia = 8;
+		mes = 8;
+		any = 1992;
+		
+		System.out.println(cognom1 + " " + cognom2 + " " + nom);
+		
+		System.out.println(dia + "/" + mes + "/" + any);
+	}
+
+}
