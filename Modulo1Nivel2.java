a1b199441907bd373db5fed7a40848f7a6a543dc
schzrodriguez <74927530+schzrodriguez@users.noreply.github.com>
2021-03-02 18:08:11 +0100

    Add files via upload

---
+++ Modulo1Nivel2.java
@@ -0,0 +1,20 @@
+
+public class Modulo1Nivel2 {
+
+	public static void main(String[] args) {
+		
+		double varDouble = 8.5121;
+		
+		int varInt;
+		float varFloat;
+		String varString;
+		
+		varInt = (int) varDouble;
+		varFloat = (float) varDouble;
+		varString = ""+varDouble;
+		
+		System.out.println(varInt + "   " +  varFloat + "   " + varString);	
+
+	}
+
+}
